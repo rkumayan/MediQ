1908e086c79d786dec209b9545f59d1c8f94b1b1
rkumayan <kumayanrahul01@gmail.com>
2025-05-07 16:10:40 +0530

    setted up server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const app = express();
+// middlewares
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('MongoDB connected successfully!');
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 5000}`);
+      });
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
