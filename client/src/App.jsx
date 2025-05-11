ab23fc05a76381a0e13736e5db695529b9d4b91f
rkumayan <kumayanrahul01@gmail.com>
2025-05-11 12:34:06 +0530

    removed clerk and created Login and Register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route  } from "react-router-dom";
 
-import Auth from "./pages/Auth.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 import './App.css'
 import Navbar from "./pages/Navbar.jsx";
 
+
+
 function App() {
 
-
+    
   return (
     <>
       
         <Navbar />
-        <BrowserRouter>
+        
           <Routes>
-            <Route path="/auth" element={<Auth />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route path="/" element={<h1>Home</h1>} />
             <Route path="/about" element={<h1>About</h1>} />
             <Route path="*" element={<h1>404 Not Found</h1>} />
           </Routes>
-        </BrowserRouter>    
+        
     </>
   )
 }
