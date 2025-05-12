161fc37c207593b1cd6b6770ea32ea4b46256f96
rkumayan <kumayanrahul01@gmail.com>
2025-05-12 23:52:58 +0530

    minor bug-fixes in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -67,8 +67,8 @@ const Home = () => {
                             onClick = { () => joinNow(department._id) }
                             >
                             Join now
-                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                             </svg>
                         </button>
                     </div>
