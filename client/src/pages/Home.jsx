dec7c1bdcc2b790da4d2fa856ea653e4be86cbbc
rkumayan <kumayanrahul01@gmail.com>
2025-05-19 09:46:46 +0530

    added bg gradient and glassmorphism in cards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -60,38 +60,37 @@ const Home = () => {
             
             { user && departments.length > 0 &&
                 departments.map((department) => (                    
-                    <div key = {department._id} className="flex m-10 p-6  bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
-                        style = {{ width: "400px", height: "300px" }}
+                    <div key = {department._id} className=" text-black backdrop-blur-md bg-white/10 border border-white/20 rounded-2xl
+                     p-6 max-w-sm  shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-300 cursor-pointer
+                     m-10 
+                    "
+                        style = {{ width: "400px"}}
                     >     
-                        <div className="">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                { department.departmentName }
-                            </h5>
-
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                { department.tagLine }
-                            </p>
-                            <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"
-                                onClick = { () => joinNow(department._id) }
-                                >
-                                Show More
-                                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                </svg>
-                            </button>
-                        </div>
-                        {/* <div className="w-1/2 bg-center"
-                            // style = {{ backgroundImage: `url(https://img.freepik.com/free-photo/mature-man-going-through-follicular-unit-extraction-process_23-2149106303.jpg)`}}
-                            // style = {{ backgroundImage: `url(https://i.pinimg.com/736x/e0/a8/45/e0a845dfb502f5db5da884830ef7a94a.jpg)`}}
-                        > */}
-                            {/* <img src = { "https://img.freepik.com/free-photo/mature-man-going-through-follicular-unit-extraction-process_23-2149106303.jpg" } alt = "Department" className = "w-full h-32 object-cover rounded-lg mt-4" /> */}
-                            {/* <img src = { "https://i.pinimg.com/736x/e0/a8/45/e0a845dfb502f5db5da884830ef7a94a.jpg" } alt = "Department" className = "w-full object-cover rounded-lg mt-4" /> */}
-                        {/* </div> */}
+                        
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                            { department.departmentName }
+                        </h5>
+                        <p className="mb-3 font-normal text-black">
+                            { department.tagLine }
+                        </p>
+                        <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"
+                            onClick = { () => joinNow(department._id) }
+                            >
+                            Show More
+                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </button>
+                    
+                      
                     </div>
       
                 ))
             }   
             </div>
+
+
+            
         </div>   
     );
 }
